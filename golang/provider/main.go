533355bf3b0a98b20d4d48916dc0aa0165ca5522
xrwang8 <xrwang8@gmail.com>
2021-08-30 16:27:01 +0800

    去除nacos 的命名空间

--- golang/provider/main.go
+++ golang/provider/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"github.com/asim/go-micro/v3"
 	"github.com/asim/go-micro/v3/logger"
 	"github.com/asim/go-micro/v3/registry"
@@ -14,8 +13,7 @@ import (
 var etcdReg registry.Registry
 
 const (
-	defaultNacosAddr      = "127.0.0.1:8848"
-	defaultNacosNamespace = "dev"
+	defaultNacosAddr = "172.30.8.225:8848"
 )
 
 func main() {
@@ -25,16 +23,8 @@ func main() {
 	if nacosAddr == "" {
 		nacosAddr = defaultNacosAddr
 	}
-	var nacosNamespace string
-	nacosNamespace = os.Getenv("NacosNamespace")
-	if nacosNamespace == "" {
-		nacosNamespace = defaultNacosNamespace
-	}
-
 	nacosRegistry := nacos.NewRegistry(func(options *registry.Options) {
 		options.Addrs = []string{nacosAddr}
-		options.Context = context.WithValue(context.Background(), &nacos.NacosNamespaceContextKey{}, nacosNamespace)
-
 	})
 	service := micro.NewService(
 		micro.Name("provider"),
